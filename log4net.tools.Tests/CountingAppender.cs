310bbf470559b48515bac7c8f6e991e24f3a1be4
Alexander Borovtsov <Alexander.Borovtsov@dataart.com>
2021-01-21 12:41:19 +0300

    Add an async forwarder
    - add unit tests

---
+++ log4net.tools.Tests/CountingAppender.cs
@@ -0,0 +1,18 @@
+using System.Threading;
+using log4net.Appender;
+using log4net.Core;
+
+namespace log4net.tools.Tests
+{
+	public class CountingAppender : AppenderSkeleton
+	{
+		private int _counter;
+
+		public int Counter => _counter;
+
+		protected override void Append(LoggingEvent logEvent)
+		{
+			Interlocked.Increment(ref _counter);
+		}
+	}
+}
---
+++ log4net.tools.Tests/ForwardingAppenderAsyncTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using log4net.Config;
+using log4net.Core;
+using Xunit;
+
+
+namespace log4net.tools.Tests
+{
+    public class ForwardingAppenderAsyncTest
+    {
+        private ForwardingAppenderAsync _forwardingAppender;
+        private CountingAppender _countingAppender;
+        private Repository.Hierarchy.Hierarchy _hierarchy;
+
+        private void SetupRepository()
+        {
+            _hierarchy = new Repository.Hierarchy.Hierarchy();
+
+            _countingAppender = new CountingAppender();
+            _countingAppender.ActivateOptions();
+
+            _forwardingAppender = new ForwardingAppenderAsync {Fix = FixFlags.Partial};
+            _forwardingAppender.AddAppender(_countingAppender);
+
+            BasicConfigurator.Configure(_hierarchy, _forwardingAppender);
+        }
+
+        [Fact]
+        public void TestSetupAppender()
+        {
+            SetupRepository();
+
+            Assert.Equal(0, _countingAppender.Counter);
+
+            ILogger logger = _hierarchy.GetLogger("test");
+            logger.Log(typeof(ForwardingAppenderAsync), Level.Warn, "Message logged", null);
+            Thread.Sleep(1000);
+
+            Assert.Equal(1, _countingAppender.Counter);
+        }
+    }
+}
